7ba1fdafd21c75e0592359b4f0c7c04ea0d3ed2c
Lombre <jesper.birch3@gmail.com>
2015-09-26 14:22:21 +0200

    Cat ai working, without accounting for the lake
    The cat ai is working, based on the angle to the mouse. It does not take
    any form of lakes into its consideration.
    
    The mouse also mostly works.

--- src/indledendeprogrammering_afleveringsopgave_1/Opgaver/CatAndMouse.java
+++ src/indledendeprogrammering_afleveringsopgave_1/Opgaver/CatAndMouse.java
@@ -13,15 +13,26 @@ import java.util.Random;
  * @author jespe_000
  */
 public class CatAndMouse {
-
     public static Random mouseMovement = new Random();
-    public static int boardSize = 0;
+    public static int boardSize = 20;
     
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = new Point(1, 0);
-        double angle = Math.toDegrees(angleTwoPoints(point1, point2));
-        angle += 1;
+        Point mouse = new Point(9, 2);
+        Point cat = new Point(4, 4);
+        while (true)
+        {            
+            System.out.println("cat: " + cat.x + "," + cat.y + ". mouse: " + mouse.x + "," + mouse.y);
+            cat = moveCat(cat, mouse);
+            if (cat.equals(mouse))
+                break;
+            cat = moveCat(cat, mouse);
+            if (cat.equals(mouse))
+                break;            
+            mouse = moveMouse(mouse);
+            if (cat.equals(mouse)) // to be removed, not needed
+                break;
+        }   
+        System.out.println("cat: " + cat.x + "," + cat.y + ". mouse: " + mouse.x + "," + mouse.y);
     }
     
     public static void runCatAndMouse(int n, int catX, int catY, int mouseX, int mouseY)
@@ -38,8 +49,10 @@ public class CatAndMouse {
              
              while (!cat.equals(mouse))
              {
+                 cat.x += 0.5; cat.y += 0.5; mouse.x += 0.5; mouse.y += 0.5;
                  mouse = moveMouse(mouse);
-                 //cat
+                 cat = moveCat(cat, mouse);
+                 cat.x -= 0.5; cat.y -= 0.5; mouse.x -= 0.5; mouse.y -= 0.5;
              }
         }
     }
@@ -49,6 +62,12 @@ public class CatAndMouse {
         int moveDirectionMouse = mouseMovement.nextInt(3);
         if (moveDirectionMouse == 0 && mouseInPosition(0, mouse))
             mouse.move(mouse.x + 1, mouse.y);
+        if (moveDirectionMouse == 1 && mouseInPosition(1, mouse))
+            mouse.move(mouse.x, mouse.y + 1);
+        if (moveDirectionMouse == 2 && mouseInPosition(2, mouse))
+            mouse.move(mouse.x - 1, mouse.y);
+        if (moveDirectionMouse == 3 && mouseInPosition(3, mouse))
+            mouse.move(mouse.x, mouse.y - 1);
         return mouse;
     }    
     
@@ -66,24 +85,51 @@ public class CatAndMouse {
         return inPosition;
     }
     
-    public static Point moveCat(Point cat)
+    public static Point moveCat(Point cat, Point mouse)
     {
+        double angle = Math.toDegrees(angleTwoPoints(cat, mouse));
+        
+        if (((angle > 337.5) && (angle < 360)) || (angle <= 22.5)) 
+            cat.move(cat.x + 1, cat.y); 
+        else if ((angle > 22.5) && (angle <= 67.5)) 
+            cat.move(cat.x + 1, cat.y + 1);   
+        else if ((angle > 67.5) && (angle <= 112.5)) 
+            cat.move(cat.x, cat.y + 1);   
+        else if ((angle > 125.5) && (angle <= 157.5)) 
+            cat.move(cat.x - 1, cat.y + 1); 
+        else if ((angle > 157.5) && (angle <= 202.5)) 
+            cat.move(cat.x - 1, cat.y); 
+        else if ((angle > 202.5) && (angle <= 247.5)) 
+            cat.move(cat.x - 1, cat.y - 1); 
+        else if ((angle > 247.5) && (angle <= 292.5)) 
+            cat.move(cat.x, cat.y - 1); 
+        else if ((angle > 292.5) && (angle <= 337.5)) 
+            cat.move(cat.x + 1, cat.y - 1); 
+        else 
+        {
+            cat = cat;
+            throw new ArithmeticException("What happened to the angle?");
+        }
         
         return cat;
     }
     
     public static double angleTwoPoints(Point point1, Point point2)
     {
-        double angle = 0;  
-        double scalarProduct = Math.abs((point1.x * point2.x) + (point1.y * point2.y));
-        double vectorLength = getVectorLength(point1) * getVectorLength(point2);        
-        angle = Math.acos(scalarProduct / vectorLength);        
-        return angle;        
+        Point PointRight = new Point(1, 0);
+           
+        double scalarProduct = (PointRight.x  * (point2.x - point1.x)) + (PointRight.y * (point2.y - point1.y));
+        double vectorLength = 1 * getVectorLength(point2, point1);        
+        double angle = Math.acos(scalarProduct / vectorLength); 
+        
+        if (point1.y > point2.y)
+            angle = (Math.PI*2) - angle;        
+        return angle;
     }
     
-    private static double getVectorLength(Point point)
+    private static double getVectorLength(Point point1, Point point2)
     {
-        double Length = Math.sqrt((point.x)*(point.x) + (point.y)*(point.y));
+        double Length = Math.sqrt((point1.x - point2.x)*(point1.x - point2.x) + (point1.y - point2.y)*(point1.y - point2.y));
         return Length;
     }
     
